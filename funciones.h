e3c0d5d9cbd73f5ba1e8bac13765c7efb45ab921
Gabrieeel27 <g.limongi27@gmail.com>
2026-06-10 14:19:04 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+
 //¿como tal hace falta poner aca las librerias? ya que despues cuando use esto dentro del codigo ya van a estar ahi, por las dudas las puse
-#define S_1 1
-#define S_2 2
-#define S_3 3
-typedef struct{
+
+/*#define S_1 1
+#define S_2 2        lo habia hecho asi porque quise hacerme el distinto pero es mas facil para reutilizar codigo si lo dejo igual que como nos lo dio
+#define S_3 3 */
+
+#defineCANT_ARTICULOS 60
+#define SUCURSAL_1 0
+#define SUCURSAL_2 1
+#define SUCURSAL_3 2
+    typedef struct{
     char descripcion[90];
-    int csl_1;  //cantidad de sucursal
-    int csl_2;
-    int csl_3;
+    int cantidad_sucursal[3]; // Posición 0 para Sucursal 1, 1 para Sucursal 2, 2 para Sucursal 3
     int total;
 }articulos_t;
 
@@ -17,3 +22,4 @@ typedef struct{
 void iniciar_articulos (articulos_t[], int cant); //deje el corchete vacio para que la funcion pueda recibir un vector cualquiera
 void imprimir_articulos (articulos_t[], int cant, int orden[]);
 void ordenar_articulos (articulos_t[], int cant, int orden[]);
+void cargar_sistema (articulos_t[], int cant, int orden[]);
