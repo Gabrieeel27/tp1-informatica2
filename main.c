4b876440c296b8ec5fa32aaf3a7bf1941546c216
Gabrieeel27 <g.limongi27@gmail.com>
2026-06-10 14:57:33 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <funciones.h>
+
+int main(){
+   printf("Bienvenido al TP n°1 de Informatica 2");
+  
+  articulos_t articulos [CANT_ARTICULOS];
+  iniciar_articulos (articulos, CANT_ARTICULOS);
+  iniciar_sistema (articulos, CANT_ARTICULOS);
+  
+printf("\n--- LISTADO ORIGINAL ---\n");
+  
+  imprimir_articulos (articulos, CANT_ARTICULOS);
+  ordenar_articulos (articulos, CANT_ARTICULOS);
+  
+    printf("\n\n###################################");
+    printf("\n##############ORDENADO################");
+    printf("\n################################### \n"); 
+  
+  return 0;
+}
